5782172bd1671b01d7023588bb6c2ce5e17fe073
Alvaro <alvaro-mendess@outlook.com>
2022-09-12 09:19:07 -0400

    Third Edit

--- index.js
+++ index.js
@@ -3,25 +3,58 @@
 var original = `<div style="border: 1px solid red">
 <div class="icon"><i class="bi bi-briefcase"></i></div>
 <h4 class="title"><a href="">TITTLE</a></h4>
-<p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+<p class="description">DESCRIPTION</p>
 </div>`
 
-fetch("https://jsonplaceholder.typicode.com/users")
+/*fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
     .then(json => {
         json.forEach(element => {
             var services = document.getElementById("Services");
             var ser = document.createElement("div");
             var content = original;
-            content = content.replace("TITTLE",element.name);
+            content = content.replace("TITTLE", element.name);
             ser.innerHTML = content;
             ser.className = "col-lg-4 col-md-6 icon-box";
 
             services.appendChild(ser);
 
         });
+    })*/
 
+/*fetch("http://localhost:3000/services")
+    .then(response => response.json())
+    .then(json => {
+        json.forEach(element => {
+            var services = document.getElementById("Services");
+            var ser = document.createElement("div");
+            var content = original;
+            content = content.replace("TITTLE", element);
+            ser.innerHTML = content;
+            ser.className = "col-lg-4 col-md-6 icon-box";
 
+            services.appendChild(ser);
+
+        });
+    })*/
+
+//For Excel   
+fetch("http://localhost:3000/services") //AJAX Asynchronous JavaScript And XML
+    .then(response => response.json())
+    .then(json => {
+        json.Services.forEach(element => {
+            var services = document.getElementById("Services");
+            var ser = document.createElement("div");
+            var content = original;
+            content = content.replace("TITTLE", element.A);
+            content = content.replace("DESCRIPTION", element.B);
+
+            ser.innerHTML = content;
+            ser.className = "col-lg-4 col-md-6 icon-box";
+
+            services.appendChild(ser);
+
+        });
     })
 
 /*for (let i = 0; i < 6; i++) {
